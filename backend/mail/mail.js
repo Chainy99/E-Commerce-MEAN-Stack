25812d74e513608c75c4de0304a0d5d9e2d61b66
Thadchayiny Murugeswaran <99892443+Chainy99@users.noreply.github.com>
2023-01-26 17:55:40 +0530

    second commit

---
+++ backend/mail/mail.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+var nodemailer = require('nodemailer');
+function sendEmail(to,otp) {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'thadch1703@gmail.com',
+            pass: process.env.EMAILPASSWORD
+        }
+    });
+    var str = "your otp = ";
+    var a =otp;
+    str+=a;
+    str += "\notp valid till 2min"
+    var mailOptions = {
+        from: 'thadch1703@gmail.com',
+        to: to,
+        subject: 'Reset Password E-Medic-Care',
+        text: str
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
+module.exports = sendEmail
